df07d0f9dbd3d3a306a836800db229afbad3d09f
Afiq Farid <afiqfaridm@gmail.com>
2023-04-06 14:52:54 +0800

    initial commit

---
+++ JS/js.js
@@ -0,0 +1,25 @@
+var index = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("slideshow");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  index++;
+  if (index > x.length) {index = 1}    
+  x[index-1].style.display = "block";  
+  setTimeout(carousel, 5000); // Change image every 2 seconds
+}
+
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
